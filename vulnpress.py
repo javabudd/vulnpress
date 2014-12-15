f71f477dc51986c911841c86075a9ee3dfc3f6d3
andybooj <andyo@booj.com>
2014-12-15 00:23:44 -0700

    Importing exploits into vulnpress to call exploit()

--- vulnpress.py
+++ vulnpress.py
@@ -1,6 +1,8 @@
 import argparse
 from exploits.exploitsql import ExploitSql
 from exploits.exploitshell import ExploitShell
+from exploits.sql import *
+from exploits.shell import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('hostname', help='URL')
@@ -11,6 +13,6 @@ if args.hostname and args.category:
 	hostname = args.hostname
 	category = args.category
 	if category == 'sql':
-		[cls.exploit(cls, hostname) for cls in vars()['ExploitSql'].__subclasses__()]
+		[cls.exploit(hostname) for cls in vars()['ExploitSql'].__subclasses__()]
 	if category == 'shell':
-		[cls.exploit(cls, hostname) for cls in vars()['ExploitShell'].__subclasses__()]
+		[cls.exploit(hostname) for cls in vars()['ExploitShell'].__subclasses__()]
